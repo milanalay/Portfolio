50e99ded7323bddf63f8478744b1090f05fe6328
milanalay <milan.metashrine@gmail.com>
2021-02-12 14:31:22 +0545

    collect static

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
 from application.views import *
 
@@ -22,3 +23,8 @@ urlpatterns = [
     path('', home_view, name='home_view'),
     path('thankyou/', thankyou, name='thankyou'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
