42fb263bf1a104fe7828ec18e4a13e2eb1dfe8ca
milanalay <milan.metashrine@gmail.com>
2021-02-12 14:33:16 +0545

    collect static updated

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from application.views import *
 
