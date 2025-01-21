760fbf0655283ff6347be84273a754968e8dcf35
Milan <kurt@Milans-MacBook-Pro.local>
2025-01-22 01:01:01 +0800

    Issue Resolved

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio
+
+    Welcome to my GitHub portfolio! I'm Milan Ale, a Programmer and a Network Engineer passionate about designing secure, efficient, and innovative network solutions.  
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 
 
@@ -23,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bgu!(%tuwqpo5aq&dbz5($5uyddv&$_@s)@@jdu+=1c1lg7mlc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
